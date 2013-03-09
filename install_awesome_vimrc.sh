b1e3a879007bd900c09ad7fdc6166d4c49be0038
aaron <aaron@aaron.(none)>
2013-03-09 21:43:06 +0800

    Modify "install_awesome_vimrc.sh".

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -5,11 +5,6 @@ echo 'set runtimepath+=~/.vim
 source ~/.vim/vimrcs/basic.vim
 source ~/.vim/vimrcs/filetypes.vim
 source ~/.vim/vimrcs/plugins_config.vim
-source ~/.vim/vimrcs/extended.vim
-
-try
-source ~/.vim/my_configs.vim
-catch
-endtry' > ~/.vimrc
+source ~/.vim/vimrcs/extended.vim' > ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
